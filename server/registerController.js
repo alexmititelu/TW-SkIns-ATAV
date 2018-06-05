00d25731db10773699119d2e01a0082ad132a1c6
alex.mititelu <blbla@yahoo.com>
2018-06-06 01:09:50 +0300

    Functional registration

--- server/registerController.js
+++ server/registerController.js
@@ -1,7 +1,7 @@
 var url = require('url');
 var fs = require('fs');
 var qs = require('querystring');
-const MongoClient = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 function renderHTML(path, response) {
     fs.readFile("../src/html/" + path, function (error, htmlContent) {
@@ -42,67 +42,94 @@ module.exports = {
                 request.on('end', function () {
                     var formData = qs.parse(requestBody);
                     console.log(formData);
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write('<!doctype html><html><head><title>response</title></head><body>');
-                    response.write('Thanks for the data!<br />User Name: ' + formData.username);
-                    response.write('<br />Password: ' + formData.Password);
-                    response.end('</body></html>');
-                    // MongoClient
-                    // .connect('mongodb://localhost:27017')
-                    // .then(client => {
-                    //  client.db('Skillz')
+
+                    // response.write('<!doctype html><html><head><title>response</title></head><body>');
+                    // response.write('Thanks for the data!<br />User Name: ' + formData.username);
+                    // response.write('<br />Password: ' + formData.password);
+                    // response.end('</body></html>');
+                    // // connection.db('Skillz')
                     //   .collection('Autentificari')
-                    //   .find({ email : result1.uname , password: result1.psw })
-                    //   .toArray()
-                    //   .then(result => {
-                    //    console.log(result)
-                    //    let myUser = result[0];
-                    //    if( result !== undefined)
-                    //     return Promise.resolve(result);
-                       
-                    //   })
-                    //   .catch(err => {
-                    //    console.log(err);
-                    //    console.log('There has been an error at promises');
-                    //   })
-                    //   .then(user => {
-                       
-                    //    if(user[0] != null)
-                    //    {
-                    //     res.writeHead(200, {'Content-type' : 'text/html'})
-                    //     res.write(user[0].username)
-                    //     res.write( '<br></br>' )
-                    //     res.write(user[0].password)
-                    //     res.end()
-                    //    }
-                    //    else
-                    //    {
-                    //     res.writeHead(200, {'Content-type' : 'text/html'})
-                    //     res.write('404402')
-                    //     res.end()
-                    //    }
-                    //   })
-                    //   .catch(err => {
-                    //    console.log(err);
-                    //    console.log('There has been an error at writing result');
-                    //   });
-                
-                    //  client.close();
-                    // })
-                    // .catch(err => {
-                    //  console.log(err);
-                    //  console.log('There has been an error connecting do db');
-                    // })
-                
-                    
-                    //   });
-                
+                    //   .insertOne({
+                    // response.redirect('/register');
+
+                    MongoClient
+                        .connect('mongodb://localhost:27017', function (error, connection) {
+                            if (error) {
+                                throw error;
+                            }
+
+                            var dbConnection = connection.db("TW_PROJECT_SkIns");
+                            var newAccount = {
+                                username: formData.username,
+                                password: formData.password,
+                                email: formData.email
+                            };
+
+                            dbConnection.collection("Conturi").insertOne(newAccount, function (error, success) {
+                                if (error) {
+                                    throw error;
+                                }
+
+                                console.log("Am adaugat un cont nou");
+
+                                var query = { username: newAccount.username };
+                                var idCont = dbConnection.collection("Conturi").find(query).toArray(function (queryError, queryResult) {
+
+                                    if (queryError) {
+                                        throw queryError;
+                                    }
+
+                                    console.log(queryResult[0]._id);
+
+
+                                    var newAccountDetails = {
+                                        cont_id: queryResult[0]._id,
+                                        first_name: formData.firstName,
+                                        last_name: formData.lastName,
+                                        telefon: formData.phone,
+                                        gender: formData.gender,
+                                        interests: formData.interests,
+                                        isSubscribed: formData.subscribe
+                                    };
+
+                                    dbConnection.collection("Utilizatori").insertOne(newAccountDetails, function (error, success) {
+                                        if (error) {
+                                            throw error;
+                                            connection.close();
+                                        }
+                                        connection.close();
+                                    });
+                                    console.log("Am adaugat si user details");
+                                    // renderHTML("createdAccount.html",response);
+                                    /**/
+
+                                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                                    response.write("Thanks");
+                                    response.end();
+
+
+                                    // fs.readFile("../src/html/createdAccount.html", function (error, htmlContent) {
+                                    //     if (error) {
+                                    //         response.writeHead(404);
+                                    //         response.write("Couldn't load HTML / not found");
+                                    //     } else {
+                                    //         response.writeHead(200, { 'Content-Type': 'text/html' })
+                                    //         response.write(htmlContent);
+                                    //     }
+                                    //     response.end();
+                                    // });
+
+                                    /**/
+                                });
+                            });
+                        });
                 });
+
             } else {
                 response.writeHead(404, 'Resource Not Found', { 'Content-Type': 'text/html' });
                 response.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
             }
-        } 
+        }
         // else {
         //     response.writeHead(405, 'Method Not Supported', { 'Content-Type': 'text/html' });
         //     return response.end('<!doctype html><html><head><title>405</title></head><body>405: Method Not Supported</body></html>');
