93d918876d28f303183fc4b54721d8709a33a8fa
BlayneStorm <blaynestormguy@gmail.com>
2018-06-09 16:04:56 +0300

    My profile functionality

---
+++ server/serverVlad.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var qs = require('querystring');
+var fs = require('fs');
+var profileController = require('./profileController');
+var assetsControllerVlad = require('./assetsControllerVlad');
+
+
+var port = 8888;
+const hostname = '127.0.0.1';
+
+const server = http.createServer(function (req, res) {
+
+    profileController.handleRequest(req,res);
+    assetsControllerVlad.handleRequest(req,res);
+    
+});
+
+server.listen(port, hostname, ()=> {
+	console.log('Server started at port ' + port);
+});
