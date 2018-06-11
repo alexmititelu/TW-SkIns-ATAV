8096fa89d237dac5da7220b7d16e77bebc2f6d9c
Teo <claw_teodor@yahoo.com>
2018-06-11 16:23:09 +0300

    Cookies for the subscribe microservice

--- server/microservicii/subscribe/subscribeServer.js
+++ server/microservicii/subscribe/subscribeServer.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var qs = require('querystring');
 var fs = require('fs');
+var Cookies = require('cookies');
 var subscribeController = require('./subscribeController');
 
 var port = 8888;
 const hostname = '127.0.0.1';
 
 const server = http.createServer(function (req, res) {
+    var cookies = new Cookies(req, res, null);
 
-    subscribeController.handleRequest(req,res);
+    subscribeController.handleRequest(req,res,cookies);
     
 });
 
