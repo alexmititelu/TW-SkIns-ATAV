29799cfefb0548df7e607c290699e01d688dc1e8
alex.mititelu <blbla@yahoo.com>
2018-06-11 21:55:01 +0300

    Output for python service

--- server/microservicii/python/microserviciu-python.js
+++ server/microservicii/python/microserviciu-python.js
@@ -20,13 +20,69 @@ function renderPythonPage(path, response) {
     });
 }
 
+
+function renderScriptResultPage(objectResult, response) {
+    /**
+     * Legenda pt status codes:
+     *  - 200, success
+     *  - 500, internal error la server
+     *  - 501, eroare la rularea fisierului pythone (poate de compilare)
+     *  - 502, internal error la baza de date
+     *  - 503, niciun rezultat asociat exercitiului ala
+     */
+    // response.writeHead(200, { 'Content-type': 'text/html' });
+    // // response.write(results[0]);
+    // response.end('<html><body bgcolor=\'#E6E6FA\'>' + JSON.stringify(jsonResult) + '</body></html>');
+    switch(objectResult.status) {
+        case 200:
+            response.writeHead(200, { 'Content-type': 'text/html' });
+
+            var finalMessage = '';
+
+            if(objectResult.userScriptResult === objectResult.expectedScriptResult) {
+                finalMessage = 'Congratulations!'
+            } else {
+                finalMessage = 'Don\'t give up! You can do it.'
+            }
+
+            var htmlResponse = '<html>'+
+            '<style>' +
+            ' p { color : #fff5c6 }' +
+            ' h4 { color : #f2d013 }' +
+            '</style>' +
+            '<body bgcolor=\'#002156\'>' +
+            '<h4>Output:</h4>' +
+            '<p>' + objectResult.userScriptResult + '/p' +
+            '<h4>Expected Output: </h4>' +
+            '<p>' + objectResult.expectedScriptResult + '/p' +
+            '<br>' +
+            '<h4>' + finalMessage + '</h4>' +
+            '</body>'+
+            '</html>'
+            response.write(htmlResponse);
+            response.end();
+            break;
+        case 500:
+            response.writeHead(500, { 'Content-type': 'text/html' });
+            response.write('Internal error at server');
+            response.end();
+            break;
+        default:
+            response.writeHead(500, { 'Content-type': 'text/html' });
+            response.write('Default error');
+            response.end();
+    }
+
+}
+
+
 var certOptions = {
     key: fs.readFileSync(path.resolve('./server.key')),
     cert: fs.readFileSync(path.resolve('./server.crt'))
-  }
+}
 
 var serverPort = 9000;
-https.createServer(certOptions,function (request, response) {
+https.createServer(certOptions, function (request, response) {
     console.log(request.url);
 
     if (request.method === 'GET') {
@@ -253,34 +309,109 @@ https.createServer(certOptions,function (request, response) {
                 });
             } else if (request.url == '/python/fileupload') {
                 var form = new formidable.IncomingForm();
-                console.log(form);
+                // console.log(form);
                 form.parse(request, function (error, fields, files) {
+                    // console.log('----------\n' + fields  );
+                    // console.log(fields.textFromForm +'\n-----------');
+                    // console.log(fields.uploadedEx +'\n-----------');
+                    // 
+                    var nrExercitiuIncarcat = fields.uploadedEx;
+
+                    var jsonResult = {
+                        "userScriptResult": '',
+                        "expectedScriptResult": '',
+                        "message": '',
+                        "status": 200
+                    };
+
                     var oldpath = files.filetoupload.path;
-                    var newpath = __dirname +'/uploaded-user-files/' + 'username2.py'; // de pus uernameu la fraer
+                    var newpath = __dirname + '/uploaded-user-files/' + 'username2.py'; // de pus usernameu la fraer
                     fs.rename(oldpath, newpath, function (error) {
                         if (error) {
                             console.log(error);
+                            jsonResult.message = 'Internal error';
+                            jsonResult.status = 500;
+                            renderScriptResultPage(jsonResult, response);
                             // throw error;
                         }
-                        
+
                         PythonShell.run('/./uploaded-user-files/username2.py', function (shellError, results) {
                             if (shellError) {
                                 console.log(shellError);
-                                response.writeHead(404, { 'content-type': 'text/html' });
-                                response.end("Error at runtime");
+                                // response.writeHead(404, { 'content-type': 'text/html' });
+                                // response.end("Error at runtime");
+                                jsonResult.message = 'Error at runtime';
+                                jsonResult.status = 501;
+                                renderScriptResultPage(jsonResult, response);
                             } else {
-                                console.log(results);
 
-                                var jsonResult = {
-                                    "result": results
-                                };
+                                jsonResult.userScriptResult = results;
 
-                                console.log(jsonResult);
-                                // jsonResult = JSON.parse(jsonResult);
+                                /** Acum verificam ce rezultat trebuia sa obtinem de la server */
 
-                                response.writeHead(200, { 'Content-type': 'text/html' });
-                                // response.write(results[0]);
-                                response.end('<html><body bgcolor=\'#E6E6FA\'>'+JSON.stringify(jsonResult)+'</body></html>');
+
+                                MongoClient
+                                    .connect('mongodb://localhost:27017', function (error, connection) {
+                                        if (error) {
+                                            jsonResult.expectedScriptResult = 'Internal error';
+                                            jsonResult.status = 502;
+                                            renderScriptResultPage(jsonResult,response);
+                                            // throw error;
+                                            console.log(error);
+                                        } else {
+
+                                            var dbConnection = connection.db("TW_PROJECT_SkIns");
+                                            var collection = dbConnection.collection("Python_Exercises");
+
+                                            var query = {
+                                                nrExercitiu: parseFloat(nrExercitiuIncarcat)
+                                            };
+
+                                            var cursor = collection.find(query);
+                                            var index = 0;
+                                            cursor.forEach(
+                                                function (doc) {
+                                                    // console.log(doc);
+                                                    // console.log(doc.enunt);
+                                                    jsonResult.expectedScriptResult = doc.rezultat;
+                                                    index++;
+                                                    // console.log(exercises);
+                                                },
+                                                function (err) {
+                                                    // dbConnection.close();
+                                                    console.log("error");
+                                                    if (index === 0) {
+                                                        jsonResult.message = 'Internal error';
+                                                        jsonResult.status = 503;
+                                                    }
+                                                    renderScriptResultPage(jsonResult,response);
+                                                    console.log('------------JSON UL FINAL --------------')
+                                                    console.log(jsonResult);
+                                                    console.log('------------JSON UL FINAL --------------')
+                                                    // console.log(exercises[0]);
+
+                                                    // var jsonData = {
+                                                    //     data: exercises[0]
+                                                    // };
+
+                                                    // console.log(JSON.stringify(jsonData));
+                                                    // response.writeHead(200, { 'Content-Type': 'application/json' });
+                                                    // response.write(JSON.stringify(jsonData));
+                                                    // response.end();
+
+                                                }
+
+                                            );
+
+                                        }
+
+                                    });
+
+                                /** Am terminat si cu rezultatul de la server */
+
+                                // response.writeHead(200, { 'Content-type': 'text/html' });
+                                // // response.write(results[0]);
+                                // response.end('<html><body bgcolor=\'#E6E6FA\'>' + JSON.stringify(jsonResult) + '</body></html>');
                             }
                         });
 
