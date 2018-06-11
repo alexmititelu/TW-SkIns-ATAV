bbafbe0fea86b5a5fc61175722bf5fe8035bf139
Teo <claw_teodor@yahoo.com>
2018-06-11 16:23:09 +0300

    Cookies for the subscribe microservice

--- server/microservicii/subscribe/subscribeController.js
+++ server/microservicii/subscribe/subscribeController.js
@@ -1,7 +1,10 @@
 var url = require('url');
 var fs = require('fs');
 var qs = require('qs');
+var Cookies = require('cookies');
+const jwt = require('jsonwebtoken');
 const MongoClient = require('mongodb').MongoClient;
+
 
 function collectRequestData(request, callback) {
     const FORM_URLENCODED = 'application/x-www-form-urlencoded';
@@ -25,7 +28,9 @@ module.exports = {
         var path = url.parse(request.url).pathname;
         console.log("Path: " + path);
 
-        const secret = 'asdkasnd@#@#das';
+        let secret = fs.readFileSync('./../../../secret.txt');
+        var cookies = new Cookies(request,response,null);
+
 
         if (request.method === "GET") {
             if (path === "/getSubscribedCourses") {
@@ -40,66 +45,49 @@ module.exports = {
 
                     var token = cookies.get('userToken');
                     var userData = jwt.verify(token,secret);
-                    var email = JSON.stringify({email : userData.email});
+                    var userId = userData._id;
                     //cookie !
 
                     //var emailData = {email : "mititelu.alex@yahoo.com"};
 
-                    dbConnection.collection('Conturi').find(emailData).toArray(function (queryError, queryResult) {
+                    console.log("UserID : " + userId);
+                    var coursesQuery = JSON.stringify({cont_id : userId});
+                    console.log("Query : " + coursesQuery);
+
+                    dbConnection.collection('Abonati').find(coursesQuery).toArray(function (queryError, queryResult) {
 
                         if (queryError) {
                             throw queryError;
                             connection.close();
                         }
-                        console.log(queryResult.length);
-                        if (queryResult.length >= 1000){ //o sa fie != 1, se presupune ca emailul e UNIC
-                            response.writeHead(418);
-                            response.end("ERROR : multiple emails");
-                            connection.close();
-                        }
-                        else{
-                            var userId = queryResult[0]._id;
-                            console.log("UserID : " + userId);
-                            var coursesQuery = JSON.stringify({cont_id : userId});
-                            console.log("Query : " + coursesQuery);
+                        var queryData = JSON.stringify(queryResult);
+                        console.log("Query result : " + queryData);
+                        var courseArr = [];
 
-                            dbConnection.collection('Abonati').find(coursesQuery).toArray(function (queryError, queryResult) {
+                        Object.keys(queryResult).forEach(function(courseObj){
+                            var courseData = JSON.stringify(queryResult[courseObj]);
+                            console.log("Course object data : " + courseData);
+                            courseArr.push(queryResult[courseObj].curs_id);
+                        });
+                        console.log("Cursuri gasite : " + courseArr);
+                            
+                        var tempQuery = JSON.stringify({_id : {$in : courseArr}});
+                        
+                        dbConnection.collection('Cursuri').find(tempQuery).toArray(function (queryError, queryResult) {
 
                                 if (queryError) {
                                     throw queryError;
                                     connection.close();
                                 }
-                                var queryData = JSON.stringify(queryResult);
-                                console.log("Query result : " + queryData);
-                                var courseArr = [];
-
-                                Object.keys(queryResult).forEach(function(courseObj){
-                                    var courseData = JSON.stringify(queryResult[courseObj]);
-                                    console.log("Course object data : " + courseData);
-                                    courseArr.push(queryResult[courseObj].curs_id);
-                                });
-                                console.log("Cursuri gasite : " + courseArr);
-                                    
-                                var tempQuery = JSON.stringify({_id : {$in : courseArr}});
-                                
-                                dbConnection.collection('Cursuri').find(tempQuery).toArray(function (queryError, queryResult) {
-
-                                        if (queryError) {
-                                            throw queryError;
-                                            connection.close();
-                                        }
-                                        console.log("Cursuri cu detalii : " + JSON.stringify(queryResult));
-                                        response.writeHead(200,{'Content-Type': 'application/json'});
-                                        response.write(JSON.stringify(queryResult));
-                                        response.end();
-                                        connection.close();
-                                        console.log("Am trimis raspuns.");
-                                    });
-                                });
-                        }
+                                console.log("Cursuri cu detalii : " + JSON.stringify(queryResult));
+                                response.writeHead(200,{'Content-Type': 'application/json'});
+                                response.write(JSON.stringify(queryResult));
+                                response.end();
+                                connection.close();
+                                console.log("Am trimis raspuns.");
+                            });
+                        });
                     });
-
-                });
             }
             else {
                 response.writeHead(404);
@@ -117,12 +105,12 @@ module.exports = {
 
                     var dbConnection = connection.db("TW_PROJECT_SkIns");
 
-                    //var token = cookies.get('userToken');
-                    //var userData = JSON.parse(jwt.verify(token,secret));
-                    //var email = {email : userData.email};
-                    //cookie + fetch pe cont_id !
+                    var token = cookies.get('userToken');
+                    var userData = jwt.verify(token,secret);
+                    var contId = userData._id;
+                    //cookie
 
-                    var contId = "5b16b59065136feeb6a37b1a";
+                    //var contId = "5b16b59065136feeb6a37b1a";
                     
                     //iau asta din REQUEST !
                     var course;
