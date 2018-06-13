00c312c4bd9dde5266ee8d3182d134415c9fd51f
Teo <claw_teodor@yahoo.com>
2018-06-13 03:47:57 +0300

    Added guitar course

---
+++ server/microservicii/guitar/guitarApi.js
@@ -0,0 +1,10 @@
+
+function initialize () {
+    // var score =
+    // var fileName = 
+        if (fileName.includes("/") || fileName.includes("\\")){
+        throw "Invalid file name.";
+    }
+    var path = "tabs/" + fileName;
+
+}
