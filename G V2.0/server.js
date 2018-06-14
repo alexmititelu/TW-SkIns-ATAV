3c8cc32412025400950728edf72b8b381182c115
tabarcea-augustus <tabarcea.augustus@gmail.com>
2018-06-14 11:15:32 +0300

    not wrong buttons anymore

--- G V2.0/server.js
+++ G V2.0/server.js
@@ -84,7 +84,15 @@ const server = https.createServer(certOptions, (req, res) => {
     languageHandler(req,res,axios,fs);
     // addOwnCourseHandler(req, res, qs, cookies);
 
-
+    if(req.method === 'GET' && req.url === '/Cookie')
+    {
+        var cookie = cookies.get('userToken');
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(cookie);
+        res.end();
+    }
 
 });
 
